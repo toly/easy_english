9a201dabeaf7457c0c95daa4cb4fe16d9e98a6b2
toly <tolytrader@gmail.com>
2014-02-18 14:55:38 +0400

    stub for main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+__author__ = 'toly'
+
+import sys
+
+
+def main():
+    """
+        main func - entry point
+    """
+    pass
+
+
+def make_arguments_parser():
+    """
+        make arguments parser and set options
+    """
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())

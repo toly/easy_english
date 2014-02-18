0941a929c3d34283d8856614936f2da93c82f7ac
toly <tolytrader@gmail.com>
2014-02-18 15:18:40 +0400

    describe algorithm main function + docstring in main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 __author__ = 'toly'
+"""
+    script for make per-page dictionaries according to user personal list of known words
+"""
 
 import sys
 
@@ -9,6 +12,25 @@ def main():
         main func - entry point
     """
     pass
+
+    # make arguments parser and parse arguments
+
+    # if not created - make work directory for personal user lists of words
+
+    # load lists of known words and "bad" (like "the", "a", etc) words
+
+    # main loop-for by pages in input file
+
+        # loop-for by words in current page
+
+            # get lemmatized word
+
+            # if in known or "bad" word - continue
+
+            # ask user about word - to known, to unknown or to "bad"
+            # process answer
+
+    # output result file
 
 
 def make_arguments_parser():
